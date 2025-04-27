884bf18ee4ed01824f4971dc921a447c5125ed39
Antoinechss <antoine.chosson@eleves.enpc.fr>
2025-04-27 14:40:23 +0200

    first commit

---
+++ manufacture.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <list>
+#include <cout>
+
+auto components = std::list<std::string>{
+    "wheel",
+    "electronics",
+    "glass",
+    "frame",
+    "engine"
+};
+
+std::cout << components << std::endl;

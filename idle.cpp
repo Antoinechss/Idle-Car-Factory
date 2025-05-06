4710fc5be71e8fe7b3a51e8afff9f84d29dbf8e1
Ferdinand Fleury-David <ferdy2004@gmail.com>
2025-05-06 10:48:58 +0200

    10:48

--- idle.cpp
+++ idle.cpp
@@ -1,7 +1,10 @@
 #include "manufacture.h"
+#include <Imagine/Graphics.h>
+using namespace Imagine;
 
 #include <iostream>
 #include <string>
+#include <sstream>  // pour std::ostringstream
 
 int main(){
     std::cout << "Welcome to the Car Factory Idle Game!" << std::endl;
@@ -10,6 +13,18 @@ int main(){
     // CREATING A NEW GAME WITH REINITIALIZED PARAMETERS
 
     Factory factory;
-    
+    openWindow(800, 800, "Imagine++ Window");
+    while (true) {
+        std::ostringstream oss;
+        oss << "Budget: $" << factory.wallet.budget;
+        drawString(20, 20, oss.str(), BLACK, 20);
+    }
+
+
+
+
+
+    click(); // attend un clic de souris
+
     return 0;
 }
